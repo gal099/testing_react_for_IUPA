dee830dc617b2c6542885c428bd1c1a18bca9307
Juan <jb@hotmail.com>
2021-07-18 14:29:44 -0300

    tarea sugerida #1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// componente Title usa el objeto `props`
-// y una propiedad definida dentro que se llama `value`
+function ArticleContainer(props) {
+  return <article>{props.children}</article>
+}
+
 function Title(props) {
-  return <h1>{props.value}</h1>
+  return <h1>{props.children}</h1>
+}
+
+function Paragraph(props) {
+  return <p>{props.children}</p>}
+
+function EnNegrita(props) {
+  return <strong>{props.children}</strong>
+}
+
+function DeColorRojo(props) {
+  return <span style={{ color: 'red' }}>{props.children}</span>
 }
 
 function App() {
-  //Uso del componente Title con el valor "Hola Mundo!"
   return (
-    <>  
-      <Title value="Hola Mundo!" />
-      <Title value="Hello World!" />
-    </>   
+    <ArticleContainer>
+      <Title>
+        Hola <i>Mundo</i>!
+      </Title>
+      <Paragraph>
+        Párrafo de mi articulo 
+        <EnNegrita>
+          <DeColorRojo> importante</DeColorRojo>
+        </EnNegrita>!
+      </Paragraph>
+    </ArticleContainer>
   );
 }
 
