074b0d104d8ee1f55bd6abca3761b11dbb26fca5
Juan <jb@hotmail.com>
2021-07-18 13:43:08 -0300

    agrego props al componente y lo reutilizo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// nuevo componente Title
-function Title() {
-  return <h1>Hola!</h1>
+// componente Title usa el objeto `props`
+// y una propiedad definida dentro que se llama `value`
+function Title(props) {
+  return <h1>{props.value}</h1>
 }
 
 function App() {
-  //Uso del componente Title
-  return <Title />;
+  //Uso del componente Title con el valor "Hola Mundo!"
+  return (
+    <>  
+      <Title value="Hola Mundo!" />
+      <Title value="Hello World!" />
+    </>   
+  );
 }
 
 ReactDOM.render(
